70e6369cf54534e131945dafbbe80f9cb053be7c
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-10-17 12:52:47 -0700

    init commit

---
+++ chats/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dashboard from "./Dashboard.js";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1> CHATS, MAN! </h1>
+      </div>
+      <Dashboard></Dashboard>
+    </div>
+  );
+}
+
+export default App;
---
+++ chats/src/Dashboard.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: '50px',
+        padding: theme.spacing(3,2)
+    },
+    flex: {
+        display: "flex"
+    },
+    chatWindow: {
+        wdith: "70%",
+        height: "300px"
+    },
+    topicsWindow: {
+        width: "30%",
+        height: "300px",
+        borderRight: "1px solid gray"
+    },
+    chatBox: {
+        width: "85%"
+    },
+    button: {
+        width: "15%"
+    }
+}))
+
+export default function Dashboard() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <Typography variant="h4" component="h4">
+                    Chat app
+                </Typography>
+                <Typography variant="h5" component="h5">
+                    Topics go here
+                </Typography>
+                <div className={classes.flex}>
+                    <div className={classes.topicsWindow}>
+
+                    </div>
+                </div>
+                <div className={classes.flex}>
+                    <div className={classes.chatWindow}>
+                        
+                    </div>
+                </div>
+            </Paper>
+        </div>
+    )
+}
